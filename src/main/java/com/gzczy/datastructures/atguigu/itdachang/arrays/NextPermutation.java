8dfa3a7e53a831cf2769c2b42763be84baf2fd75
chenzhengyu <944989036@qq.com>
2021-03-02 11:31:58 +0800

    #31 下一个排列 优化复盘

--- src/main/java/com/gzczy/datastructures/atguigu/itdachang/arrays/NextPermutation.java
+++ src/main/java/com/gzczy/datastructures/atguigu/itdachang/arrays/NextPermutation.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 /**
  * @Description #31 下一个排列
  * @Author chenzhengyu
+ * https://leetcode-cn.com/problems/next-permutation/
  * @Date 2021-01-14 11:32
  */
 public class NextPermutation {
 
     public static void main(String[] args) {
-        int [] test = new int[]{1,2,3};
+        int[] test = new int[]{1, 2, 3};
         nextPermutation(test);
         System.out.println(Arrays.toString(test));
     }
@@ -23,7 +24,7 @@ public class NextPermutation {
         int k = n - 2;
 
         // 1、为什么要等于也要继续while循环？答案：因为当数值等于的时候相当于点是平的（相同），相当于
-        // 直接调换这2个数是无法得到比它大的数，所以是需要继续往后找的
+        // 直接调换这2个数是无法得到比它大的数，所以是需要继续往后找的（配合笔记查看图形更容易理解）
 
         while (k >= 0 && nums[k] >= nums[k + 1])
             k--;
@@ -31,8 +32,10 @@ public class NextPermutation {
         //2、找到k 就是需要调整的最高位
 
         //2.1、 如果k = -1 说明所有数都是降序排列，这时候我们需要反转作为升序排列
+        // 但是我们这里会发现 整体程序的时间复杂度为o(n)，却因为这个排序是o(n*logn)影响了效率
+        // 所以我们这里再次进行优化
         if (k == -1) {
-            Arrays.sort(nums);
+            reverse(nums, 0, n - 1);
             return;
         }
 
@@ -43,20 +46,26 @@ public class NextPermutation {
             i++;
 
         //2.3、交换 i - 1 和 k的位置上面的数值
-        int temp = nums[k];
-        nums[k] = nums[i - 1];
-        nums[i - 1] = temp;
+        swap(nums, k, i - 1);
 
         //2.4、交换完成后，从当前k高位后面所有数进行排序（升序排列）
-        int start = k + 1;
-        int end = n - 1;
+        reverse(nums, k + 1, n - 1);
+    }
+
+    //定义一个翻转数组的方法
+    private static void reverse(int[] nums, int start, int end) {
         while (start < end) {
-            int tmp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = tmp;
+            swap(nums, start, end);
             start++;
             end--;
         }
+    }
+
+    //定义一个方法 交换数组中的两个元素
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
 }
