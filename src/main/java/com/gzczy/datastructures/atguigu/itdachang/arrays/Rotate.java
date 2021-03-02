d7980ce5ac243dbf958fced871f0bd0531bf4bce
chenzhengyu <944989036@qq.com>
2021-03-02 15:57:52 +0800

    #48 旋转图像 优化复盘

--- src/main/java/com/gzczy/datastructures/atguigu/itdachang/arrays/Rotate.java
+++ src/main/java/com/gzczy/datastructures/atguigu/itdachang/arrays/Rotate.java
@@ -5,23 +5,17 @@ import java.util.Arrays;
 /**
  * @Description #48 旋转图像
  * @Author chenzhengyu
+ * https://leetcode-cn.com/problems/rotate-image/
  * @Date 2021-01-14 13:14
  */
 public class Rotate {
 
     public static void main(String[] args) {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        //Demo1(matrix);
-        rotate(matrix);
-        printlnArray(matrix);
-    }
-
-    private static void Demo1(int[][] matrix) {
-        System.out.println("=======原始数组矩阵Demo1========");
-        printlnArray(matrix);
         MathMethod(matrix);
+        //rotate(matrix);
+        //转置后结果
         printlnArray(matrix);
-        System.out.println("=======Demo1结束========");
     }
 
     /**
@@ -48,7 +42,6 @@ public class Rotate {
         //输出进行反转的数组查看
         System.out.println("=======转置矩阵======");
         printlnArray(matrix);
-        System.out.println("=========结果=========");
         //2、翻转行 以每一行中间作为转换的中界线 进行前后下标互换即可
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n / 2; j++) {
@@ -63,14 +56,15 @@ public class Rotate {
 
     /**
      * 方法二：分治（分为四部分旋转）
-     *
+     * 时间复杂度：O(N^2) 是两重循环的复杂度。
+     * 空间复杂度：O(1) 由于我们在一次循环中的操作是“就地”完成的，并且我们只用了长度为 4 的临时列表做辅助
      * @param matrix
      */
     public static void rotate2(int[][] matrix) {
         //1、只需要遍历1/4矩阵，左上角开始 需要只考虑一次中间值 行考虑了 就列不用考虑
         // 固定中间值不变，其他都要旋转
         int n = matrix.length;
-        // n % 2 基数+1 偶数不加
+        // n % 2 基数+1 偶数不加 只处理1/4 然后其他通过位置对应关系进行推断；然后做行的时候考虑中间，列则无需考虑
         for (int i = 0; i < n / 2 + n % 2; i++) {
             // 左上1/4
             for (int j = 0; j < n / 2; j++) {
@@ -82,7 +76,6 @@ public class Rotate {
                 int col = j;
                 // 4维数组作为案例
                 //观察图片发现行列转换的规律：row + newCol = n - 1; col = newCol
-                //(1,0)-14 -> (0,2)-2 -> (2,3)-9 -> (3,1)-7 -> (1,0)-14
                 for (int k = 0; k < 4; k++) {
                     //保存当前的行和列
                     tmp[k] = matrix[row][col];
@@ -101,18 +94,20 @@ public class Rotate {
                     row = col;
                     col = n - 1 - x;
                 }
-
             }
         }
     }
 
-    // 方法三：改进
+    /**
+     * 方法三：改进
+     * 时间复杂度：O(N^2)，是两重循环的复杂度。
+     * 空间复杂度：O(1)。我们在一次循环中的操作是“就地”完成的。
+      */
     public static void rotate(int[][] matrix) {
         int n = matrix.length;
         // 旋转 1/4 个矩阵 n+1/2是为了防止出现奇数矩阵的情况
         // 假如是5*5矩阵 中间元素不动，那就是需要遍历24个元素 需要（25-1）/4 = 6 作为 1/4 的矩阵进行推导运算
         for (int i = 0; i < (n + 1) / 2; i++) {
-            //
             for (int j = 0; j < n / 2; j++) {
                 int temp = matrix[i][j];
                 //不断的将上一个位置的元素往前面挪动
@@ -120,6 +115,8 @@ public class Rotate {
                 matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
                 matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
                 matrix[j][n - i - 1] = temp;
+                //Debug
+                //printlnArray(matrix);
             }
         }
     }
@@ -128,5 +125,6 @@ public class Rotate {
         for (int i = 0; i < matrix.length; i++) {
             System.out.println(Arrays.toString(matrix[i]));
         }
+        System.out.println("=====================");
     }
 }
