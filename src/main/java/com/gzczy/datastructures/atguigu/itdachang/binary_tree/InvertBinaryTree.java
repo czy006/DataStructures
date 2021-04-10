61d8dbfce09752e43205a3b1828f9fb7b24cefeb
chenzhengyu <944989036@qq.com>
2021-04-10 16:44:37 +0800

    # 226 反转二叉树 补充

--- src/main/java/com/gzczy/datastructures/atguigu/itdachang/binary_tree/InvertBinaryTree.java
+++ src/main/java/com/gzczy/datastructures/atguigu/itdachang/binary_tree/InvertBinaryTree.java
@@ -1,7 +1,9 @@
 package com.gzczy.datastructures.atguigu.itdachang.binary_tree;
 
+import java.util.LinkedList;
+
 /**
- * @Description 反转二叉树
+ * @Description 反转二叉树 #226
  * 输入：
  *      4
  *    /   \
@@ -77,6 +79,34 @@ public class InvertBinaryTree {
         TreeNode right = invertTree2(root.right);
         root.left = right;
         root.right = left;
+        return root;
+    }
+
+    /**
+     * 循环实现
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree3(TreeNode root){
+        if(root == null) return null;
+        //将二叉树中的节点逐层放入队列中，再迭代处理队列中的元素
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            //每次都从队列中拿一个节点，并交换这个节点的左右子树
+            TreeNode temp = queue.poll();
+            TreeNode left = temp.left;
+            temp.left = temp.right;
+            temp.right = left;
+            //如果当前节点的左子树不为空，则放入队列等待后续处理
+            if (root.left != null){
+                queue.add(root.left);
+            }
+            //如果当前节点的右子树不为空，则放入队列等待后续处理
+            if (root.left != null){
+                queue.add(root.right);
+            }
+        }
         return root;
     }
 
